438c1748b82a9c86a7358189559379cc1547e221
formaggia <luca.formaggia@polimi.it>
2025-01-25 13:41:34 +0000

    corrected a bug which is no ta bug

--- 09_numpy_cpp-custom-matrix/matrix.h
+++ 09_numpy_cpp-custom-matrix/matrix.h
@@ -2,143 +2,186 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 
-template <class T>
-class Matrix
+template <class T> class Matrix
 {
+private:
+  std::vector<T>      m_data;    // data array
+  std::vector<size_t> m_shape;   // number of entries in each dimensions
+  std::vector<size_t> m_strides; // stride length for each index
 
-  private:
+public:
+  // default constructor
+  // -------------------
 
-    std::vector<T>      m_data;     // data array
-    std::vector<size_t> m_shape;    // number of entries in each dimensions
-    std::vector<size_t> m_strides;  // stride length for each index
+  Matrix(){};
 
-  public:
+  // constructor
+  // -----------
 
-    // default constructor
-    // -------------------
+  Matrix(const std::vector<size_t> &shape, const T *data = NULL)
+  {
+    if(shape.size() < 1 || shape.size() > 3)
+      throw std::runtime_error("Input should be 1-D, 2-D, or 3-D");
 
-    Matrix<T>(){};
+    // store 'm_strides' and 'm_shape' always in 3-D,
+    // use unit-length for "extra" dimensions (> 'shape.size()')
+    while(m_shape.size() < 3)
+      {
+        m_shape.push_back(1);
+      }
+    while(m_strides.size() < 3)
+      {
+        m_strides.push_back(1);
+      }
 
-    // constructor
-    // -----------
+    for(int i = 0; i < shape.size(); i++)
+      m_shape[i] = shape[i];
 
-    Matrix<T>(const std::vector<size_t> &shape, const T *data=NULL)
-    {
-      if ( shape.size()<1 || shape.size()>3 )
-        throw std::runtime_error("Input should be 1-D, 2-D, or 3-D");
+    m_strides[0] = m_shape[2] * m_shape[1];
+    m_strides[1] = m_shape[2];
+    m_strides[2] = 1;
 
-      // store 'm_strides' and 'm_shape' always in 3-D,
-      // use unit-length for "extra" dimensions (> 'shape.size()')
-      while ( m_shape  .size()<3 ) { m_shape  .push_back(1); }
-      while ( m_strides.size()<3 ) { m_strides.push_back(1); }
+    int size = m_shape[0] * m_shape[1] * m_shape[2];
 
-      for ( int i=0 ; i<shape.size() ; i++ )
-        m_shape[i] = shape[i];
+    for(int i = 0; i < m_data.size(); i++)
+      m_data[i] = (T)0;
 
-      m_strides[0] = m_shape[2]*m_shape[1];
-      m_strides[1] = m_shape[2];
-      m_strides[2] = 1;
+    while(m_data.size() < size)
+      m_data.push_back((T)0);
 
-      int size = m_shape[0]*m_shape[1]*m_shape[2];
+    if(data != NULL)
+      for(int i = 0; i < size; i++)
+        m_data[i] = data[i];
+  };
 
-      for ( int i=0 ; i<m_data.size() ; i++ )
-        m_data[i] = (T)0;
+  // copy constructor
+  // ----------------
 
-      while ( m_data.size()<size )
-        m_data.push_back((T)0);
+  Matrix(const Matrix<T> &) = default;
+  Matrix<T> &operator=(const Matrix<T> &) = default;
 
-      if ( data!=NULL )
-        for ( int i=0 ; i<size ; i++ )
-          m_data[i] = data[i];
-    };
+  // index operators
+  // ---------------
 
-    // copy constructor
-    // ----------------
+  T &
+  operator[](size_t i)
+  {
+    return m_data[i];
+  };
+  const T &
+  operator[](size_t i) const
+  {
+    return m_data[i];
+  };
 
-    Matrix               (const Matrix<T> &) = default;
-    Matrix<T>& operator= (const Matrix<T> &) = default;
+  T &
+  operator()(size_t h, size_t i = 0, size_t j = 0)
+  {
+    return m_data[h * m_strides[0] + i * m_strides[1] + j * m_strides[2]];
+  };
 
-    // index operators
-    // ---------------
+  const T &
+  operator()(size_t h, size_t i = 0, size_t j = 0) const
+  {
+    return m_data[h * m_strides[0] + i * m_strides[1] + j * m_strides[2]];
+  };
 
-    T&       operator[](size_t i)       { return m_data[i]; };
-    const T& operator[](size_t i) const { return m_data[i]; };
+  // iterators
+  // ---------
 
-    T& operator()(size_t h, size_t i=0, size_t j=0)
-    { return m_data[h*m_strides[0]+i*m_strides[1]+j*m_strides[2]]; };
+  auto
+  begin()
+  {
+    return m_data.begin();
+  }
+  auto
+  begin() const
+  {
+    return m_data.begin();
+  }
+  auto
+  end()
+  {
+    return m_data.end();
+  }
+  auto
+  end() const
+  {
+    return m_data.end();
+  }
 
-    const T& operator()(size_t h, size_t i=0, size_t j=0) const
-    { return m_data[h*m_strides[0]+i*m_strides[1]+j*m_strides[2]]; };
+  // return pointer to data
+  // ----------------------
 
-    // iterators
-    // ---------
+  const T *
+  data(void) const
+  {
+    return m_data.data();
+  };
 
-    auto begin()       { return m_data.begin(); }
-    auto begin() const { return m_data.begin(); }
-    auto end()         { return m_data.end();   }
-    auto end() const   { return m_data.end();   }
+  // return shape array [ndim]
+  // -------------------------
 
-    // return pointer to data
-    // ----------------------
+  std::vector<size_t>
+  shape(size_t ndim = 0) const
+  {
+    if(ndim == 0)
+      ndim = this->ndim();
 
-    const T* data(void) const { return m_data.data(); };
+    std::vector<size_t> ret(ndim);
 
-    // return shape array [ndim]
-    // -------------------------
+    for(size_t i = 0; i < ndim; ++i)
+      ret[i] = m_shape[i];
 
-    std::vector<size_t> shape(size_t ndim=0) const
-    {
-      if ( ndim == 0 )
-        ndim = this->ndim();
+    return ret;
+  };
 
-      std::vector<size_t> ret(ndim);
+  // return strides array [ndim]
+  // ---------------------------
 
-      for ( size_t i = 0 ; i < ndim ; ++i )
-        ret[i] = m_shape[i];
+  std::vector<size_t>
+  strides(bool bytes = false) const
+  {
+    size_t              ndim = this->ndim();
+    std::vector<size_t> ret(ndim);
 
-      return ret;
-    };
+    for(size_t i = 0; i < ndim; ++i)
+      ret[i] = m_strides[i];
 
-    // return strides array [ndim]
-    // ---------------------------
+    if(bytes)
+      for(size_t i = 0; i < ndim; ++i)
+        ret[i] *= sizeof(T);
 
-    std::vector<size_t> strides(bool bytes=false) const
-    {
-      size_t ndim = this->ndim();
-      std::vector<size_t> ret(ndim);
+    return ret;
+  };
 
-      for ( size_t i = 0 ; i < ndim ; ++i )
-        ret[i] = m_strides[i];
+  // return size
+  // -----------
 
-      if ( bytes )
-        for ( size_t i = 0 ; i < ndim ; ++i )
-          ret[i] *= sizeof(T);
+  size_t
+  size(void) const
+  {
+    return m_data.size();
+  };
 
-      return ret;
-    };
+  // return number of dimensions
+  // ---------------------------
 
-    // return size
-    // -----------
+  size_t
+  ndim(void) const
+  {
+    size_t i;
 
-    size_t size ( void ) const { return m_data.size(); };
+    for(i = 2; i > 0; i--)
+      if(m_shape[i] != 1)
+        break;
 
-    // return number of dimensions
-    // ---------------------------
-
-    size_t ndim ( void ) const
-    {
-      size_t i;
-
-      for ( i = 2 ; i > 0 ; i-- )
-        if ( m_shape[i] != 1 )
-          break;
-
-      return i+1;
-    };
+    return i + 1;
+  };
 
 }; // class Matrix
 
